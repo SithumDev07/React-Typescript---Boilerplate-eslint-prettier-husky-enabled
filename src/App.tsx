6d637a64a3b8925b141e4c7603edce993cad1fb3
SithumDev07 <sithum.dashantha@gmail.com>
2022-05-13 08:00:55 +0530

    created homepage and added some another eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Eslint, Husky, Prettier, TypeScript } from "./Components/Icons";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-5xl text-gray-800">Hello React</h1>
+    // eslint-disable-next-line max-len
+    <div className="App text-blue-600 flex items-center flex-col text-8xl w-[80%] h-[80vh] border rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500 justify-center">
+      <div className="flex items-center">
+        <h1>React Js</h1>
+        <i className="bi bi-balloon-fill ml-3" />
+      </div>
+      <div className="flex items-center space-x-6 mt-10">
+        <TypeScript />
+        <Eslint />
+        <Prettier />
+        <Husky />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Icons/index.js
@@ -0,0 +1,4 @@
+export { default as Eslint } from "./Eslint";
+export { default as Husky } from "./Husky";
+export { default as Prettier } from "./Prettier";
+export { default as TypeScript } from "./TypeScript";
